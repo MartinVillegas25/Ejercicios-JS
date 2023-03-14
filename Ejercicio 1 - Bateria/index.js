30b76b49d0ad49ff36abb2baafcf3c876ef6dd00
MartinVillegas25 <martinvillegas90@hotmail.com>
2023-03-14 16:00:43 -0300

    ejercicio 1

---
+++ Ejercicio 1 - Bateria/index.js
@@ -0,0 +1,36 @@
+
+
+const key = document.querySelectorAll('.key');
+
+
+
+
+const keys = [];
+
+    
+
+      window.addEventListener('keydown', (e)=>{
+         key.forEach(k=>{
+            let num = k.getAttribute('data-key');
+            const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+            
+  
+            if(e.keyCode == num){
+                k.classList.add('playing');
+                audio.play();
+                setTimeout(() => {
+                    k.classList.remove('playing');
+                }, 200);
+                
+            }else{
+                console.log('no esta');
+            }
+            
+        })
+   })
+    
+        
+
+
+
+ 
